7eef830c81126fb7cd08f73aff6e45427a694d8c
Shifana-123 <144646481+Shifana-123@users.noreply.github.com>
2024-01-21 20:48:05 +0530

    Add files via upload

---
+++ test/AddUsersPageTest.java
@@ -0,0 +1,103 @@
+package org.qalegend.test;
+import org.qalegend.automationcore.Base;
+import org.qalegend.constants.Constants;
+import org.qalegend.constants.Messages;
+import org.qalegend.page.*;
+import org.qalegend.retryanalyzer.RetryAnalyzer;
+import org.qalegend.utilities.ExcelUtility;
+import org.qalegend.utilities.RandomUtility;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import java.util.ArrayList;
+
+
+
+public class AddUsersPageTest extends Base {
+    @Test(groups="Sanity")
+    public void verifyAddUserPageTitle() {
+        LoginPage login = new LoginPage(driver);
+        ArrayList<String> data = ExcelUtility.readData(Constants.TEST_DATA_EXCEL_PATH, Constants.LOGIN_PAGE);
+        String username = data.get(1);
+        login.enterUserName(username);
+        String password = data.get(2);
+        login.enterPassWord(password);
+        login.clickOnLoginButtonElement();
+        HomePage home = login.clickOnEndTourButton();
+        UserManagementPage usermanagement=home.clickOnUserManagement();
+        UsersPage userPage =usermanagement.clickOnUsersOption();
+        AddUsersPage addPage = userPage.clickOnAddButton();
+        String actualUserPageTitle = addPage.getTitle();
+        ArrayList<String> userPageData = ExcelUtility.readData(Constants.TEST_DATA_EXCEL_PATH, Constants.ADD_USER_PAGE);
+        String expectedUserPageTitle = userPageData.get(1);
+        Assert.assertEquals(actualUserPageTitle, expectedUserPageTitle, Messages.TITLE_MISMATCH);
+    }
+    @Test(groups = "Smoke")
+    public void verifyAddUser() {
+        String firstName = RandomUtility.getFirstName();
+        String lastName = RandomUtility.getLastName();
+        String emailID = firstName + "." + lastName + "123@yahoo.com";
+        String password = firstName + "." + lastName;
+        LoginPage login = new LoginPage(driver);
+        ArrayList<String> data = ExcelUtility.readData(Constants.TEST_DATA_EXCEL_PATH, Constants.LOGIN_PAGE);
+        String loginUsername = data.get(1);
+        login.enterUserName(loginUsername);
+        String loginPassword = data.get(2);
+        login.enterPassWord(loginPassword);
+        login.clickOnLoginButtonElement();
+        HomePage home = login.clickOnEndTourButton();
+        UserManagementPage userManagement=home.clickOnUserManagement();
+        UsersPage userPage = userManagement.clickOnUsersOption();
+        AddUsersPage adduser = userPage.clickOnAddButton();
+        adduser.enterFirstName(firstName);
+        adduser.enterLastName(lastName);
+        adduser.enterEmailId(emailID);
+        adduser.enterUserName(firstName);
+        adduser.enterPassWord(password);
+        adduser.enterconfirmPassWord(password);
+        UsersPage users=adduser.clickOnSaveButton();
+        users.enterTextOnSearchField(emailID);
+        String actualLoggedEmailID=users.getTextFromLoggedUsersMailID();
+        String expectedLoggedEmailID=emailID;
+        Assert.assertEquals(actualLoggedEmailID,expectedLoggedEmailID,Messages.NEW_USER_ADDED_FAILED);
+
+    }
+
+    @Test(groups = "Smoke")
+    public void verifyUserLoginWithNewlyAddedUser() {
+        LoginPage login = new LoginPage(driver);
+        ArrayList<String> data = ExcelUtility.readData(Constants.TEST_DATA_EXCEL_PATH, Constants.LOGIN_PAGE);
+        String loginUsername = data.get(1);
+        login.enterUserName(loginUsername);
+        String loginPassword = data.get(2);
+        login.enterPassWord(loginPassword);
+        login.clickOnLoginButtonElement();
+        HomePage home=login.clickOnEndTourButton();
+        UserManagementPage userManagement=home.clickOnUserManagement();
+        UsersPage userPage = userManagement.clickOnUsersOption();
+        AddUsersPage adduser = userPage.clickOnAddButton();
+        ArrayList<String> userDatas=ExcelUtility.readData(Constants.TEST_DATA_EXCEL_PATH,Constants.ADD_USER_PAGE);
+        String firstName=userDatas.get(2);
+        adduser.enterFirstName(firstName);
+        String lastName=userDatas.get(3);
+        adduser.enterLastName(lastName);
+        String emailID=userDatas.get(4);
+        adduser.enterEmailId(emailID);
+        String userName=userDatas.get(2);
+        adduser.enterUserName(userName);
+        String passWord=userDatas.get(5);
+        adduser.enterPassWord(passWord);
+        String confirmPassWord=userDatas.get(5);
+        adduser.enterconfirmPassWord(confirmPassWord);
+        UsersPage usersPage=adduser.clickOnSaveButton();
+        usersPage.clickOnUsersLoggedUserName();
+        LoginPage page=usersPage.clickOnSignOutButton();
+        String newUserNameToLogin=userDatas.get(2);
+        login.enterUserName(newUserNameToLogin);
+        String newPasswordToLogin=userDatas.get(5);
+        login.enterPassWord(newPasswordToLogin);
+        HomePage homePage=login.clickOnLoginButtonElement();
+        String actualTextOfAddedUser=homePage.getTextNewlyAddedUserName();
+        String expectedTextOfAddedUser=firstName+" " +lastName;
+        Assert.assertEquals(actualTextOfAddedUser,expectedTextOfAddedUser,Messages.NEW_USER_ADDED_FAILED);
+    }
+}
---
+++ test/UserManagementPageTest.java
@@ -0,0 +1,41 @@
+package org.qalegend.test;
+
+import org.openqa.selenium.WebElement;
+import org.qalegend.automationcore.Base;
+import org.qalegend.constants.Constants;
+import org.qalegend.constants.Messages;
+import org.qalegend.page.HomePage;
+import org.qalegend.page.LoginPage;
+import org.qalegend.page.UserManagementPage;
+import org.qalegend.retryanalyzer.RetryAnalyzer;
+import org.qalegend.utilities.AppUtility;
+import org.qalegend.utilities.ExcelUtility;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.qalegend.utilities.ExcelUtility.readData;
+
+public class UserManagementPageTest extends Base {
+        @Test(groups = "Smoke")
+        public void verifyUserManagementOptions() {
+                LoginPage login = new LoginPage(driver);
+                ArrayList<String> data = ExcelUtility.readData(Constants.TEST_DATA_EXCEL_PATH, Constants.LOGIN_PAGE);
+                String username = data.get(1);
+                login.enterUserName(username);
+                String password = data.get(2);
+                login.enterPassWord(password);
+                login.clickOnLoginButtonElement();
+                HomePage home = login.clickOnEndTourButton();
+                UserManagementPage userManagement = home.clickOnUserManagement();
+                List<String> actualUserManagementOptions=userManagement.getUserManagementOptions();
+                List<String> values = ExcelUtility.readData(Constants.TEST_DATA_EXCEL_PATH, Constants.USER_MANAGEMENT_PAGE);
+                Assert.assertEquals(actualUserManagementOptions,values,Messages.USER_MANAGEMENT_OPTIONS_NOT_VISIBLE);
+                }
+        }
+
+
+
+
